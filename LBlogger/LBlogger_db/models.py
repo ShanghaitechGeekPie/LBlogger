dde8629bb5d2ac5a227029476b55f71a04255bdc
eastpiger <lvwt@shanghaitech.edu.cn>
2015-03-07 17:57:26 +0800

    s

---
+++ LBlogger/LBlogger_db/models.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,verbose_name = '账户')
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255,verbose_name = '名称')
+
+class Post(models.Model):
+    title = models.CharField(max_length=255,verbose_name = '标题')
+    content = models.TextField(verbose_name = "内容")
+    author = models.ForeignKey(User,verbose_name = '作者')
+    tag = models.ManyToManyField(Tag,verbose_name = '标签')
